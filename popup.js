cdef32597430fe5df1948645f2ad47f2c2b59124
plaeski <lisa.plaseski@gmail.com>
2016-06-02 22:37:36 -0400

    Added rating feature

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     if ($('#files').val().length===0){
       chrome.storage.sync.get(null, function(res){
         $("#files").val(res.raw);
+        setCurrent(res.position);
       });
     }
 
@@ -12,48 +13,13 @@ var urlList = [];
 var position = 0;
 var totalLength = 0;
 
-$('#previous').on('click', function(){
-  chrome.storage.sync.get(null, function(res){
-    position = res.position;
-    if(position>0){
-      position--;
-      chrome.storage.sync.set({
-        'position': position
-      }, function(){
-        printDetails(res.value);
-      });
-    }
-  })
-});
-
-$('#next').on('click', function(){
-  chrome.storage.sync.get(null, function(res){
-    position = res.position
-    totalLength = res.value.length;
-    if(position<(totalLength-1)){
-      position++;
-      chrome.storage.sync.set({
-        'position': position
-      }, function(){
-        printDetails(res.value);
-      });
-    }
-  })
-});
-
 function printDetails(url){
   chrome.storage.sync.get(null, function(res){
-    console.log(res);
     chrome.tabs.update(null, {
       url: 'http://' + url[res.position]
     });
   });
 }
-
-//   chrome.tabs.update(null, {
-//     url: 'http://'+url[position]
-// });
-  // window.location.assign('http://' + url[position])
 
 function handleFileSelect(evt) {
   chrome.storage.sync.clear();
@@ -69,8 +35,98 @@ function selectUrls(file) {
     }
     chrome.storage.sync.set({
       'raw': file,
+      'raw_array': data,
       'value': urlList,
       'position': position
     })
     printDetails(urlList);
 }
+
+function setCurrent(position){
+  var current = parseInt(position) + 1
+  $("#current").text(current);
+}
+
+// Navigation
+$('#previous').on('click', function(){
+  chrome.storage.sync.get(null, function(res){
+    position = res.position;
+    if(position>0){
+      position--;
+      setCurrent(position);
+      chrome.storage.sync.set({
+        'position': position
+      }, function(){
+        printDetails(res.value);
+      });
+    } else {
+      alert("This is the first website");
+    }
+  })
+});
+
+$('#next').on('click', function(){
+  chrome.storage.sync.get(null, function(res){
+    position = res.position
+    totalLength = res.value.length;
+    if(position<(totalLength-1)){
+      position++;
+      setCurrent(position);
+      chrome.storage.sync.set({
+        'position': position
+      }, function(){
+        printDetails(res.value);
+      });
+    } else {
+      alert("This is the last site");
+    }
+  })
+});
+
+// Ratings
+
+$("#okay").on("click", function(){
+  chrome.storage.sync.get(null, function(res){
+    console.log(res);
+    position = res.position;
+    var raw = res['raw_array'];
+    raw[position].push('Y');
+    chrome.storage.sync.set({
+      'raw_array': raw
+    });
+  })
+})
+
+$("#bad").on("click", function(){
+  chrome.storage.sync.get(null, function(res){
+    console.log(res);
+    position = res.position;
+    var raw = res['raw_array'];
+    raw[position].push('N');
+    chrome.storage.sync.set({
+      'raw_array': raw
+    });
+  })
+})
+
+$("#grey").on("click", function(){
+  chrome.storage.sync.get(null, function(res){
+    console.log(res);
+    position = res.position;
+    var raw = res['raw_array'];
+    raw[position].push('Grey');
+    chrome.storage.sync.set({
+      'raw_array': raw
+    });
+  })
+})
+
+// Get Results
+$("#finish").on("click", function(){
+  chrome.storage.sync.get(null, function(res){
+    var raw = res.raw_array
+    var joined = raw.join("\n");
+    $("#finishedData").show();
+    $("#finishedData").val(joined);
+  });
+})
